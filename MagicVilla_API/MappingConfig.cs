0c4f6a5ed00cb56b08d146502b37c7814f3f66b1
Manuel <manuelcapricornio12@hotmail.com>
2023-12-08 03:43:17 -0500

    DTO y AutoMapper

---
+++ MagicVilla_API/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MagicVilla_API.Modelos;
+using MagicVilla_API.Modelos.Dto;
+
+namespace MagicVilla_API
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            // 1era forma
+            CreateMap<Villa, VillaDto>();
+            CreateMap<VillaDto, Villa>();
+
+            // 2da forma
+            CreateMap<Villa, VillaCreateDto>().ReverseMap();
+            CreateMap<Villa, VillaUpdateDto>().ReverseMap();
+        }
+    }
+}
