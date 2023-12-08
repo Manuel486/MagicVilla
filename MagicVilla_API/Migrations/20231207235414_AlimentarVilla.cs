14e1422b8e4adcc0185936d86c018f6835fd5b2b
Manuel <manuelcapricornio12@hotmail.com>
2023-12-08 01:54:36 -0500

    DbContext - Inyeccion de dependencia

---
+++ MagicVilla_API/Migrations/20231207235414_AlimentarVilla.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MagicVilla_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlimentarVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Detale",
+                table: "Villas",
+                newName: "Detalle");
+
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenidad", "Detalle", "FechaActualizacion", "FechaCreacion", "ImagenUrl", "MetrosCuadrados", "Nombre", "Ocupantes", "Tarifa" },
+                values: new object[,]
+                {
+                    { 1, "", "Detalle de la Villa...", new DateTime(2023, 12, 7, 18, 54, 14, 352, DateTimeKind.Local).AddTicks(5986), new DateTime(2023, 12, 7, 18, 54, 14, 352, DateTimeKind.Local).AddTicks(5963), "", 50, "Villa Real", 5, 200.0 },
+                    { 2, "", "Detalle de la Villa...", new DateTime(2023, 12, 7, 18, 54, 14, 352, DateTimeKind.Local).AddTicks(5994), new DateTime(2023, 12, 7, 18, 54, 14, 352, DateTimeKind.Local).AddTicks(5993), "", 40, "Premiun Vista a la piscina", 4, 150.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.RenameColumn(
+                name: "Detalle",
+                table: "Villas",
+                newName: "Detale");
+        }
+    }
+}
--- MagicVilla_API/obj/Debug/net7.0/MagicVilla_API.AssemblyInfo.cs
+++ MagicVilla_API/obj/Debug/net7.0/MagicVilla_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbde3ee66c2ad88e8c8be61887be973ea77959b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
